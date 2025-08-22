8ab185ef0860bbda99efa06df82561944c72fb12
Abdulkareemhaif <abdulkareem.hussain.08@gmail.com>
2025-08-22 23:51:24 +0300

    Add environment configuration and initial data files
    - Introduced .env file for environment variables including DATABASE_URL and FLASK_SECRET_KEY.
    - Updated app.py and config.py to load environment variables and fallback to SQLite if DATABASE_URL is not set.
    - Added initial CSV files for student grades and salary data.
    - Created new __pycache__ files for compiled Python modules.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from app import create_app
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
